e3184e0cfb6ca01ef218db4b9c07aa175c2fed65
Mike Clancy <mclancy96@gmail.com>
2025-02-19 16:24:33 -0500

    empty function setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+
+}
