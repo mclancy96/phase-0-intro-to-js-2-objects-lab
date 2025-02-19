a79cb6089d0f898d55cccfebee0f5738cf966d3b
Mike Clancy <mclancy96@gmail.com>
2025-02-19 16:29:15 -0500

    adds non-destructive update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your solution in this file!
-const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+const employee = {};
 
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
 }
 
 const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
